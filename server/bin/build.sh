82ebf515ba2b5635ead6dc3e825ac9fdb82d6da6
Yoannmont <y.mont@orange.fr>
2025-07-19 15:53:25 +0000

    test

--- server/bin/build.sh
+++ server/bin/build.sh
@@ -1,8 +1,6 @@
 echo installing dependencies
-pip3.12 install -r ../requirements.lock
+pip install -r ../requirements.lock
 
-echo activating env
-source ../env/bin/activate
 
 echo collecting staticfiles
 python3.12 ../manage.py collectstatic --no-input --clear
