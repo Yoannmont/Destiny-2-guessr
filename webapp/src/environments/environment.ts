976b081263755904f1e236346b85032c6079e79a
Yoannmont <y.mont@orange.fr>
2025-07-20 18:32:42 +0000

    moving backend to render

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SERVER_BASE_URL: 'https://d2guessr.duckdns.org',
+  SERVER_BASE_URL: 'https://destiny-2-guessr.onrender.com',
   WEBSITE_BASE_URL: 'https://d2guessr.netlify.app',
 };
