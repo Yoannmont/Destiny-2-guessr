f0349e1c8e452e417899cb7e56ea42bf9b13454b
Yoann Monteiro <y.mont@orange.fr>
2026-09-21 22:01:37 +0200

    env: update env

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SERVER_BASE_URL: 'https://d2guessr-prod.duckdns.org',
+  SERVER_BASE_URL: 'https://d2guessr-prod.yomax.fr',
   WEBSITE_BASE_URL: 'https://d2guessr.netlify.app',
 };
